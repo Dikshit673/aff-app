c106a6405face67029b81c203543105de451297e
dikshit673 <sachinnavodian@gmail.com>
2025-07-26 16:15:12 +0530

    added lazy loading

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,27 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
-import Login from '../pages/Login';
-import Dashboard from '../pages/Dashboard';
-import Products from '../pages/Products';
 import { AuthGuard, PublicGuard } from '../features/auth/AuthGuard';
 import Navbar from '@/components/Navbar';
+import { lazy, Suspense } from 'react';
+
+const Login = lazy(() => import('../pages/Login'));
+const Dashboard = lazy(() => import('../pages/Dashboard'));
+const Products = lazy(() => import('../pages/Products'));
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        {/* public routes */}
-        <Route element={<PublicGuard />}>
-          <Route path='/login' element={<Login />} />
-        </Route>
-        {/* private routes */}
-        <Route element={<AuthGuard />}>
-          <Route path='/' element={<Navigate to='/dashboard' replace />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/products' element={<Products />} />
-        </Route>
-        <Route path='*' element={<h1>404</h1>} />
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          {/* public routes */}
+          <Route element={<PublicGuard />}>
+            <Route path='/login' element={<Login />} />
+          </Route>
+          {/* private routes */}
+          <Route element={<AuthGuard />}>
+            <Route path='/' element={<Navigate to='/dashboard' replace />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/products' element={<Products />} />
+          </Route>
+          <Route path='*' element={<h1>404</h1>} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
